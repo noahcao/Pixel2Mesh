e9902be3d728e5c943fb9388ec09f38070be171c
ultmaster <scottyugochang@hotmail.com>
2019-07-22 00:29:08 +0800

    refine vgg classifier

--- models/classifier.py
+++ models/classifier.py
@@ -9,12 +9,27 @@ class Classifier(nn.Module):
         super(Classifier, self).__init__()
 
         self.nn_encoder, self.nn_decoder = get_backbone(options)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.fc = nn.Linear(list(self.nn_encoder.children())[-1].out_channels, num_classes)
+        if "vgg" in options.backbone:
+            self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+            self.classifier = nn.Sequential(
+                nn.Linear(list(self.nn_encoder.children())[-1].out_channels * 7 * 7, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(True),
+                nn.Dropout(),
+                nn.Linear(4096, num_classes),
+            )
+        elif "resnet" in options.backbone:
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            self.classifier = nn.Linear(self.nn_encoder.inplanes, num_classes)
+        else:
+            raise NotImplementedError
 
     def forward(self, img):
-        features = self.nn_encoder(img)[-1]  # last layer
-        pooling = self.avgpool(features)
-        output = self.fc(pooling.reshape(pooling.size(0), -1))
-        return output
+        x = self.nn_encoder(img)[-1]  # last layer
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
