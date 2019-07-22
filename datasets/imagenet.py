61cd8a4720205390ed36df0f8d4d467a4d124a71
ultmaster <scottyugochang@hotmail.com>
2019-07-23 00:29:38 +0800

    add data augmentation to imagenet training

--- datasets/imagenet.py
+++ datasets/imagenet.py
@@ -22,13 +22,22 @@ class ImageNet(Dataset):
                 self.images.append(image)
                 self.labels.append(int(label))
 
-        self.transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-        ])
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                              std=[0.229, 0.224, 0.225])
+        if split == "train":
+            self.transform = transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                self.normalize
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                self.normalize
+            ])
 
     def __getitem__(self, index):
         image = Image.open(os.path.join(self.image_dir, self.images[index]))
