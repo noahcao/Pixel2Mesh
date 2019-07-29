a0f157a812653a95a06a7153a8ce6dfeafc16a51
ultmaster <scottyugochang@hotmail.com>
2019-07-29 08:50:29 +0800

    train with checkpoint

---
+++ slurm/train_checkpoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [[ $# -lt 4 ]] ; then
+    echo 'too few arguments supplied'
+    exit 1
+fi
+
+PARTITION=$1
+NAME=$2
+OPTIONS=$3
+CHECKPOINT=$4
+
+srun -p ${PARTITION} \
+    --job-name=Mesh \
+    --gres=gpu:8 \
+    --ntasks=1 \
+    --kill-on-bad-exit=1 \
+    python entrypoint_train.py --name ${NAME} --options ${OPTIONS} --checkpoint ${CHECKPOINT} &
